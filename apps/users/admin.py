f3635dd3e3f706850213f31286e7f29f15012867
Mila <makhliyoabboskhonova@gmail.com>
2025-10-21 11:39:38 +0500

    .

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-
-from products.models import Category, Product
+from django.contrib.admin import ModelAdmin
 from users.models import User
 
 
 @admin.register(User)
-class UsersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'phone')
+class UserAdmin(ModelAdmin):
+    model = User
+    list_display = ('id', 'phone', 'email', 'is_active', 'is_staff')
+    ordering = ('phone',)
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
