741d45a309173154b54706ae29bf1d3f394027f0
Kholmumin <xolmomin@gmail.com>
2025-10-30 12:33:41 +0500

    add task

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -2,22 +2,21 @@ from products.models import Category, Product
 from rest_framework.serializers import ModelSerializer
 
 
-
 class CategoryModelSerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name', 'slug','icon','parent')
+        fields = ('name', 'slug', 'icon', 'parent')
 
 
 class ProductModelSerializer(ModelSerializer):
-    category= CategoryModelSerializer()
+    category = CategoryModelSerializer()
+
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price', 'image','slug','category')
+        fields = ('name', 'description', 'price', 'image', 'slug', 'category')
+
 
 class ProductDetailModelSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price', 'image','slug','description','category')
-
-
+        fields = ('id', 'name', 'price', 'image', 'slug', 'description', 'category')
