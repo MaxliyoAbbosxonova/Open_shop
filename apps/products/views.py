4e1d7c77a3c20f69d6b5e7fb4f4c52d8268ac081
Mila <makhliyoabboskhonova@gmail.com>
2025-11-11 10:54:44 +0500

    .

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,10 +1,10 @@
-from products.models import Category, Highlight, Product
+from products.models import Category, Highlight, Product, Cart, OrderItem, CartItem
 from django_filters.rest_framework import DjangoFilterBackend
 from products.serializers import (
     CategoryModelSerializer,
     HighlightModelSerializer,
     ProductDetailModelSerializer,
-    ProductModelSerializer,
+    ProductModelSerializer, CartModelSerializer, OrderItemModelSerializer, CartItemModelSerializer,
 )
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -17,10 +17,9 @@ class ProductListAPIView(ListCreateAPIView):
     serializer_class = ProductModelSerializer
     pagination_class = CustomPageNumberPagination
     permission_classes = (AllowAny,)
-    filter_backends = [DjangoFilterBackend,OrderingFilter]
-    ordering_fields = ('price', '-price','created_at','-created_at')
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ('price', '-price', 'created_at', '-created_at')
     ordering = ('-created_at',)
-
 
 
 class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
@@ -31,10 +30,30 @@ class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
 class CategoryListAPIView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
-    permission_classes = [AllowAny,]
+    permission_classes = [AllowAny, ]
     pagination_class = CustomPageNumberPagination
 
 
 class HighlightListAPIView(ListCreateAPIView):
     queryset = Highlight.objects.filter().order_by('-created_at')
     serializer_class = HighlightModelSerializer
+
+
+class CartListAPIView(ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartModelSerializer
+
+
+class CartItemListAPIView(ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemModelSerializer
+
+
+class OrderListAPIView(ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemModelSerializer
+
+
+class OrderItemListAPIView(ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemModelSerializer
