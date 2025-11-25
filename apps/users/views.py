51a6e69b2a64f2a9379f4261018ad332b3284ed7
Mila <makhliyoabboskhonova@gmail.com>
2025-11-25 11:21:18 +0500

    new

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,8 +4,10 @@ import redis
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from users.models import User
 from users.serializers import SendSmsCodeSerializer, VerifySmsCodeSerializer
-from users.utils import check_sms_code, random_code, send_sms_code,_get_login_key
+from users.utils import check_sms_code, random_code, send_sms_code, _get_login_key
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
@@ -33,4 +35,5 @@ class LoginAPIView(APIView):
         is_valid_code = check_sms_code(phone, code)
         if not is_valid_code:
             return Response({"message": "invalid code"}, status.HTTP_400_BAD_REQUEST)
+
         return Response(serializer.get_data)
